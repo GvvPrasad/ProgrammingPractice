67990c890bb49439cb383689795f12a872e05e1f
Prasad <varaprasad1819@gmail.com>
2024-11-08 00:11:17 +0530

    Strings

---
+++ src/main/java/string/DisplaySting.java
@@ -0,0 +1,39 @@
+package string;
+
+import java.util.Iterator;
+
+public class DisplaySting {
+
+	public static void main(String[] args) {
+		
+		//when values are same there will be only single object
+		//string as object, string literal
+		String s1 = "prasad";
+		String s2 = "prasad";
+		
+		//string with 'new' keyword
+		// here two objects will be created
+		String s3 = new String("prasad");
+		String s4 = new String("prasad");
+		
+		//Split
+		String word ="Prasad is a QA Engineer";
+		
+		//split by word space
+		String[] splitedArray = word.split(" ");
+		for(String SW: splitedArray) {
+			System.out.println(SW);
+		}
+		
+		//Dislay by letter
+		for (int i = 0; i < word.length(); i++) {
+			System.out.println(word.charAt(i));
+		}
+		System.out.println("***");
+		//Display string in reverse order
+		for(int i=word.length()-1;i>=0;i--) {
+			System.out.print(word.charAt(i));
+		}
+	}
+
+}
